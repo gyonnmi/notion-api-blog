7aa55d76a24fc9e31844f2780f8f9734b7a0dd3d
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-11-21 22:51:18 +0900

    Add: Detail Page

---
+++ pages/blog/[pageId].tsx
@@ -0,0 +1,51 @@
+import { getDatabaseItems, getPageContent } from 'cms/notion';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { ExtendedRecordMap } from 'notion-types';
+import React from 'react';
+
+interface BlogDetailsPageProps {
+  recordMap: ExtendedRecordMap;
+}
+
+const BlogDetailsPage = ({ recordMap }: BlogDetailsPageProps) => {
+  return <div>BlogDetailsPage</div>;
+};
+
+export default BlogDetailsPage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const pageId = params?.pageId; //const pageId: string | string[] | undefined
+
+  // pageId가 undefind일 경우 예외 처리(타입가드)
+  if (!pageId) throw Error('PageId is not defind');
+  // const pageId: string | string[]
+
+  const recordMap = await getPageContent(pageId.toString());
+
+  return {
+    props: {
+      recordMap,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const databaseId = process.env.DATABASE_ID;
+
+  if (!databaseId) throw new Error('DATABASE_ID is not defind');
+
+  const databaseItems = await getDatabaseItems(databaseId);
+
+  const paths = databaseItems.map(({ id: pageId }) => {
+    return {
+      params: {
+        pageId,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
