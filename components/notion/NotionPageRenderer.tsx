9dd102398eb687ee00520047db4f7c8839d024ec
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-12-04 22:45:20 +0900

    Add: NotionRenderer

--- components/notion/NotionPageRenderer.tsx
+++ components/notion/NotionPageRenderer.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
+import TagItem from 'components/card/tags/TagItem';
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import Link from 'next/link';
 import { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
 
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then((m) => m.Code)
+);
+const Collection = dynamic(() =>
+  import('react-notion-x/build/third-party/collection').then(
+    (m) => m.Collection
+  )
+);
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+);
+
 interface NotionPageRendererProps {
   recordMap: ExtendedRecordMap;
+  isProfile?: boolean;
 }
 
-const NotionPageRenderer = ({ recordMap }: NotionPageRendererProps) => {
-  return <NotionRenderer recordMap={recordMap} />;
+const NotionPageRenderer = ({
+  recordMap,
+  isProfile,
+}: NotionPageRendererProps) => {
+  const LinkObject = isProfile
+    ? {}
+    : {
+        Link,
+      };
+
+  return (
+    <NotionRenderer
+      recordMap={recordMap}
+      fullPage={true}
+      disableHeader={true}
+      showTableOfContents={true}
+      components={{
+        Code,
+        Collection,
+        Equation,
+        Image,
+        propertyDateValue: (dateProperty) =>
+          dateProperty.data[0][1][0][1].start_date,
+        propertySelectValue: ({ option: { id, color, value } }) => (
+          <TagItem key={id} color={color} name={value} />
+        ),
+        ...LinkObject,
+      }}
+    />
+  );
 };
 
 export default NotionPageRenderer;
